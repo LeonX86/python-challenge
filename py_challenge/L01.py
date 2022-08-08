2c0fce3f2ae13cbd4497751ccd2080c07fd62581
Leon <49100008+LeonX86@users.noreply.github.com>
2022-08-08 22:51:34 +0800

    Delete py_challenge directory

--- py_challenge/L01.py
+++
@@ -1,2 +0,0 @@
-#http://www.pythonchallenge.com/pc/def/0.html
-print(2**38)
--- py_challenge/L05/L05.py
+++
@@ -1,8 +0,0 @@
-#http://www.pythonchallenge.com/pc/def/peak.html
-import pickle
-fname = "./L05/banner.p"
-with open(fname,'rb') as fs:
-    data=pickle.load(fs)
-
-for line in data:
-    print("".join(x[0]*x[1] for x in line))
